8b7c7e614e2f9a1cde7ad7a4b9bc04cf0653925d
weidong <2405320456@qq.com>
2022-12-26 23:22:58 +0800

    修改vue.config.js与原有的一样，页面可以访问了（不持续加载/白屏）

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,142 @@
-const { defineConfig } = require('@vue/cli-service')
+'use strict'
+// const {defineConfig} = require('@vue/cli-service')
+
 const path = require('path')
+const defaultSettings = require('./src/settings.js')
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+    return path.join(__dirname, dir)
 }
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      scss: {
-        additionalData: `@import "@/assets/styles/index.scss";`
-        //����ȫ�ֱ���
-      }
-    }
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        "@": resolve("src")
-      },
-      fallback: {
-        path: false
-      }
+const name = defaultSettings.title || 'MyBlog管理系统' // 标题
+
+const port = process.env.port || process.env.npm_config_port || 8088 // 端口
+
+
+module.exports = {
+    // transpileDependencies: true,
+    css: {  //???sass????
+        loaderOptions: {
+            scss: {
+                additionalData: `@import "@/assets/styles/index.scss";`
+                //??????????
+            }
+        }
     },
-  }
-})
+    publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+    // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
+    outputDir: 'dist',
+    // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+    assetsDir: 'static',
+    // 是否开启eslint保存检测，有效值：ture | false | 'error'
+    lintOnSave: process.env.NODE_ENV === 'development',
+    // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+    productionSourceMap: false,
+    // webpack-dev-server 相关配置
+    devServer: { // webpack-dev-server ???????
+        host: '0.0.0.0',
+        port: port,
+        proxy: {
+            // detail: https://cli.vuejs.org/config/#devserver-proxy
+            [process.env.VUE_APP_BASE_API]: {
+                target: `http://localhost:8080`,   //??????
+                changeOrigin: true,
+                pathRewrite: {
+                    // ['^' + process.env.VUE_APP_BASE_API]: ''
+                    ['http://127.0.0.1:8080/' + process.env.VUE_APP_BASE_API]: ''
+                }
+            }
+        },
+        historyApiFallback: true, //??webpack 5 ??disableHostCheck????????
+        allowedHosts: "all"  //
+    },
+    configureWebpack: {
+        name: name,
+        resolve: {
+            alias: {
+                "@": resolve("src")
+            },
+            fallback: {
+                // path: false
+                path: require.resolve("path-browserify")
+            }
+        }
+    },
+    chainWebpack(config) {
+        config.plugins.delete('preload') // TODO: need test
+        config.plugins.delete('prefetch') // TODO: need test
+
+        // set svg-sprite-loader
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/assets/icons'))
+            .end()
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/assets/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+            .end()
+
+        // set preserveWhitespace
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+                options.compilerOptions.preserveWhitespace = true
+                return options
+            })
+            .end()
+
+        config
+            // https://webpack.js.org/configuration/devtool/#development
+            .when(process.env.NODE_ENV === 'development',
+                config => config.devtool('cheap-source-map')
+            )
+
+        config
+            .when(process.env.NODE_ENV !== 'development',
+                config => {
+                    config
+                        .plugin('ScriptExtHtmlWebpackPlugin')
+                        .after('html')
+                        .use('script-ext-html-webpack-plugin', [{
+                            // `runtime` must same as runtimeChunk name. default is `runtime`
+                            inline: /runtime\..*\.js$/
+                        }])
+                        .end()
+                    config
+                        .optimization.splitChunks({
+                        chunks: 'all',
+                        cacheGroups: {
+                            libs: {
+                                name: 'chunk-libs',
+                                test: /[\\/]node_modules[\\/]/,
+                                priority: 10,
+                                chunks: 'initial' // only package third parties that are initially dependent
+                            },
+                            elementUI: {
+                                name: 'chunk-elementUI', // split elementUI into a single package
+                                priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                                test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                            },
+                            commons: {
+                                name: 'chunk-commons',
+                                test: resolve('src/components'), // can customize your rules
+                                minChunks: 3, //  minimum common number
+                                priority: 5,
+                                reuseExistingChunk: true
+                            }
+                        }
+                    })
+                    config.optimization.runtimeChunk('single')
+                }
+            )
+    }
+}
